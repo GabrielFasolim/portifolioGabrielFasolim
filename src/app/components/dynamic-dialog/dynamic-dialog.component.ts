27557b43220a1a8b0ca30e8a1b87398636bdb511
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-10-31 22:55:15 -0300

    criação do card

--- src/app/components/dynamic-dialog/dynamic-dialog.component.ts
+++ src/app/components/dynamic-dialog/dynamic-dialog.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef, DialogService } from 'primeng/dynamicdialog';
+import * as details from '../../../assets/details.json';
 
 @Component({
   selector: 'app-dynamic-dialog',
   templateUrl: './dynamic-dialog.component.html',
-  styleUrls: ['./dynamic-dialog.component.css']
+  styleUrls: ['./dynamic-dialog.component.scss']
 })
 export class DynamicDialogComponent implements OnInit {
 
     ref: DynamicDialogRef | undefined;
     data: any;
+
+    details: any = (details as any).default.details;
 
     constructor(public dialogService: DialogService) {
 
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import * as projetos from '../../../assets/ss.json';
+import * as projetos from '../../../assets/cards.json';
+import * as details from '../../../assets/details.json';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { DynamicDialogComponent } from '../dynamic-dialog/dynamic-dialog.component';
 
@@ -14,6 +15,7 @@ export class ProjectsComponent {
   constructor(private dialog: DialogService) { }
 
   projects: any = (projetos as any).default.projetos;
+  details: any = (details as any).default.detalhes;
 
   open(data: any) {
     let ref = this.dialog.open(DynamicDialogComponent, {
