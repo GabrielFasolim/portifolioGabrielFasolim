872777e05e98bfe2e0857fc379bcdf154f23bf3f
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-11-01 15:02:24 -0300

    update cards

--- src/app/components/dynamic-dialog/dynamic-dialog.component.ts
+++ src/app/components/dynamic-dialog/dynamic-dialog.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { DynamicDialogRef, DialogService } from 'primeng/dynamicdialog';
-import * as details from '../../../assets/details.json';
+import { DynamicDialogRef, DialogService, DynamicDialogConfig } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-dynamic-dialog',
   templateUrl: './dynamic-dialog.component.html',
   styleUrls: ['./dynamic-dialog.component.scss']
+
 })
 export class DynamicDialogComponent implements OnInit {
 
-    ref: DynamicDialogRef | undefined;
     data: any;
 
-    details: any = (details as any).default.details;
+    details: any;
 
-    constructor(public dialogService: DialogService) {
-
+    fecharDialog() {
+      if (this.ref) {
+        this.ref.close(); // Fecha o diálogo se a referência existir
+      }
     }
+    constructor(public ref: DynamicDialogRef, public config: DynamicDialogConfig) { 
+      console.log("Data: " + JSON.stringify(config.data.employee));
+      this.details = config.data;
+      console.log(config.data);
+  }
+    
 
   ngOnInit(): void {
-
+  
+    }
   }
 
-}
